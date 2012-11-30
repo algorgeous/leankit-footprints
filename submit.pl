b4ee17aa03eab60b30d06a5d54591880174af25b
Alfred Bacon <bacon@gatech.edu>
2012-11-30 16:48:26 -0500

    Passing in 3 arguments to script.

--- submit.pl
+++ submit.pl
@@ -8,6 +8,13 @@ require LWP::UserAgent;
 use strict;
 use warnings;
 
+die "Invalid arguments: $0 <footprints server> <workspace id> <incident id>" unless $#ARGV == 2;
+
+# Get arguments
+my $footprints_server = $ARGV[0];
+my $workspace = $ARGV[1];
+my $incident_id = $ARGV[2];
+
 # BoardID from LeanKit
 my $boardid = 33696023;
 # LaneID from LeanKit
@@ -16,9 +23,9 @@ my $authentication = 'Basic bGVhbmtpdC1hcGlAcGUuZ2F0ZWNoLmVkdTpRbThqZ2thbnM2';
 my $server = 'gtpe.leankitkanban.com';
 my $uri = "http://$server/Kanban/Api/Board/$boardid/AddCard/Lane/$laneid/Position/0";
 
-my $title = "FootPrints Incident via Perl #$$";
+my $title = "FootPrints Incident via Perl #$incident_id";
 my $description = 'Globally parallel task intermandated schemas after magnetic core competencies. Seamlessly benchmark equity invested intellectual capital rather than resource sucking strategic.';
-my $json = "{ 'Title': \"$title\", 'Description': \"$description\", 'TypeId': 33732759, 'Priority': 1, 'Size': 0, 'IsBlocked': false, 'ExternalSystemName': 'GT ServiceDesk', 'ExternalSystemUrl': 'http://servicedesk.gatech.edu/', 'ExternalCardID': $$ }";
+my $json = "{ 'Title': \"$title\", 'Description': \"$description\", 'TypeId': 33732759, 'Priority': 1, 'Size': 0, 'IsBlocked': false, 'ExternalSystemName': 'GT ServiceDesk', 'ExternalSystemUrl': 'http://$footprints_server/', 'ExternalCardID': $incident_id }";
 
 my $req = HTTP::Request->new( 'POST', $uri );
 $req->header( Authorization => $authentication );
@@ -28,7 +35,7 @@ $req->content( $json );
 my $lwp = LWP::UserAgent->new;
 my $response = $lwp->request( $req );
 if ($response->is_success) {
-   print $response->decoded_content;  # or whatever
+   print $response->decoded_content;
 } else {
   die $response->status_line;
 }
